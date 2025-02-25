86e07191ef04536402e3ac5c68dd5ebddb626ecf
root <root@ip-172-31-11-239.eu-west-2.compute.internal>
2025-02-25 15:39:42 +0000

    latest

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Navigate to the deployment directory
-cd /var/www/lottery
-
-# Remove specific files if necessary (be cautious)
-sudo rm -f package.json
-sudo rm -f firebase-messaging-sw.ts
-sudo rm -r src/*
+cd /var/www/lottery && shopt -s extglob dotglob && rm -rf !(competition|.*)
