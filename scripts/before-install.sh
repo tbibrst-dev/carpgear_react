b1cc816b4479be95e3a1c5cfc0529f825bef19a0
root <root@ip-172-31-11-239.eu-west-2.compute.internal>
2025-02-25 14:53:07 +0000

    latest

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 # Navigate to the deployment directory
 cd /var/www/lottery
 
-# Remove the specific file causing the issue
+# Remove specific files if necessary (be cautious)
+sudo rm -f package.json
 sudo rm -f firebase-messaging-sw.ts
