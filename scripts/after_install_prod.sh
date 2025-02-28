87275fc12d6b6e9bd2dd9e7495153bbb2fb90ef2
root <root@ip-172-31-11-239.eu-west-2.compute.internal>
2025-02-28 10:48:29 +0000

    latest

--- scripts/after_install_prod.sh
+++ scripts/after_install_prod.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 cd /var/www/lottery
 
-#Installing_Node
+# Installing Node dependencies
+echo "Installing Node dependencies..."
 /usr/bin/npm install
+
+echo "Building React app..."
 /usr/bin/node run build
 
 # Copy React build to Apache server root
+echo "Copying React build to Apache server root..."
 sudo cp -r dist/* /var/www/html/
+
+echo "Changing ownership of files in /var/www..."
 sudo chown -R ubuntu:www-data /var/www/*
 
 # Restart services
+echo "Restarting Apache2 service..."
 sudo systemctl restart apache2
+
+echo "Deployment complete!"
