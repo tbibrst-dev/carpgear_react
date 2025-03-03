92b6d26598be9facb61533db1d3b59a691e196b1
root <root@ip-172-31-11-239.eu-west-2.compute.internal>
2025-03-03 09:55:40 +0000

    latest

--- competition/wp-content/plugins/competitions/class.competitions-admin.php
+++ competition/wp-content/plugins/competitions/class.competitions-admin.php
@@ -4103,6 +4103,8 @@ class Competitions_Admin
                             // $reward_link = admin_url('admin.php?page=reward_prizes_entrants&id=' . $competition['id'] . '&reward=' . $reward_record['id'] . '&limit=' . $limit);
                             $reward_link = FRONTEND_URL. "competition/details/". $competition['title'] ."-" .$competition['id']; 
 
+                            error_log("reward_link". print_r($reward_link,true));
+
                             $mailSent = 0;
 
                             $mailer = WC()->mailer();
